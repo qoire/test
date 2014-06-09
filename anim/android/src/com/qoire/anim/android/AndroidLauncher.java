71f7d3bae85f5e8960b4c6c99d597ad91fb65efe
qoire <mr.sun.yao@gmail.com>
2014-06-09 10:26:18 -0400

    first commit

---
+++ anim/android/src/com/qoire/anim/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.qoire.anim.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.qoire.anim.anim;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new anim(), config);
+	}
+}
---
+++ anim/desktop/src/com/qoire/anim/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.qoire.anim.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.qoire.anim.anim;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new anim(), config);
+	}
+}
