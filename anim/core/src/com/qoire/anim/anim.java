4c88cdb6d8ae59f95b52e0c71889211b25882a1e
qoire <mr.sun.yao@gmail.com>
2014-06-09 12:03:13 -0400

    added data folder

--- anim/core/src/com/qoire/anim/anim.java
+++ anim/core/src/com/qoire/anim/anim.java
@@ -4,24 +4,40 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class anim extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+    private SpriteBatch batch;
+    private TextureAtlas textureAtlas;
+    private Animation animation;
+    private float elapsedTime = 0;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        batch = new SpriteBatch();
+        textureAtlas = new TextureAtlas(Gdx.files.internal("spritesheet.atlas"));
+        animation = new Animation(1/15f, textureAtlas.getRegions());
 	}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        textureAtlas.dispose();
+    }
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+
+        //begin drawing
+        elapsedTime += Gdx.graphics.getDeltaTime();
+        batch.draw(animation.getKeyFrame(elapsedTime, true), 0, 0);
+        batch.end();
 	}
 }
